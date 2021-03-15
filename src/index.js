77015724805bff84c08419740b2efa9e2d65e777
gbrietzig <57928418+gbrietzig@users.noreply.github.com>
2021-03-15 00:24:28 -0300

    In edit todo

--- src/index.js
+++ src/index.js
@@ -48,15 +48,40 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline,
+    created_at: new Date()
+  }
+
+  user.todos.push(todo)
+
+  return response.status(201).send();
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  todo = user.todos.find(todo => user.todos.id === id);
+  console.log(todo)
+  todo.title = title
+  todo.deadline = deadline
+  todos.splice(todo, 1);
+
+  return response.status(200).json(todos);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
@@ -67,9 +92,10 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.get('/', (request, response) => {
-  console.log('oi')
-  return response.json(users)
+app.get('/', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  return response.json(user.todos)
 });
 
 module.exports = app;
